12b8173aebb68bcda7bb3db469ffc8d0b94b39de
Omar El Jraouy <oel-jrao@c2r1p8.1337.ma>
2025-02-23 10:35:24 +0000

    -

--- mlx-training/train.c
+++ mlx-training/train.c
@@ -6,3 +6,32 @@
 #define WIN_WIDTH 800
 #define WIN_HEIGHT 600
 
+typedef struct s_data
+{
+    void *mlx;
+    void *win;
+    void *img;
+    int img_width;
+    int img_height;
+} t_data;
+
+int handle_keypress(int keycode, t_data *data)
+{
+    if (keycode == XK_Escape)
+    {
+        mlx_destroy_image(data->mlx, data->win);
+        exit(0);
+    }
+    return 0;
+}
+
+int main()
+{
+    t_data data;
+
+    // inintialize MLX
+    data.mlx = mlx_init();
+    if (!data.mlx) return 1;
+
+    // load an image(ensure you have )
+}
